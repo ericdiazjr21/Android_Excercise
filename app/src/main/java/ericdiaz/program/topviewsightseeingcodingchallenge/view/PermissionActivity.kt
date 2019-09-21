4bd14eab0aa664bbe67ef25efe76ac916479c82a
Eric Diaz <ericdiaz@pursuit.org>
2019-09-21 19:49:03 -0400

    added permission logic to permisison activity

--- app/src/main/java/ericdiaz/program/topviewsightseeingcodingchallenge/view/PermissionActivity.kt
+++ app/src/main/java/ericdiaz/program/topviewsightseeingcodingchallenge/view/PermissionActivity.kt
@@ -1,11 +1,48 @@
 package ericdiaz.program.topviewsightseeingcodingchallenge.view
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.Intent
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat.checkSelfPermission
 
-class PermissionActivity : AppCompatActivity(){
+class PermissionActivity : AppCompatActivity() {
+
+    private val permissionRequestId = 23
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        checkLocationPermission()
+    }
+
+    private fun checkLocationPermission() {
+        if (checkSelfPermission(this, ACCESS_FINE_LOCATION) == PERMISSION_GRANTED) {
+            startWeatherActivity()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(ACCESS_FINE_LOCATION),
+                permissionRequestId
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if ((grantResults.isNotEmpty()) && grantResults[0] == PERMISSION_GRANTED) {
+            startWeatherActivity()
+        }
+        finish()
+    }
+
+    private fun startWeatherActivity() {
+        startActivity(Intent(this, WeatherActivity::class.java))
+        finish()
     }
 }
