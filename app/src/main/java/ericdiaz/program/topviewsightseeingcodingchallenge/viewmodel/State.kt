f6b98867ddf1ce87221f6b060118ea6af7659cb3
Eric Diaz <ericdiaz@pursuit.org>
2019-09-21 18:25:55 -0400

    updated state desciptor to object

--- app/src/main/java/ericdiaz/program/topviewsightseeingcodingchallenge/viewmodel/State.kt
+++ app/src/main/java/ericdiaz/program/topviewsightseeingcodingchallenge/viewmodel/State.kt
@@ -5,25 +5,23 @@ import ericdiaz.program.topviewsightseeingcodingchallenge.model.WeatherResponse
 sealed class State {
     data class Success(
         val weatherResponse: WeatherResponse,
-        val stateDescriptor: StateDescriptor
+        val stateDescriptor: String
     ) : State()
 
     data class Failure(
         val throwable: Throwable,
-        val stateDescriptor: StateDescriptor
+        val stateDescriptor: String
     ) : State()
 
     object Loading : State()
 }
 
-class StateDescriptor() {
+object StateDescriptor {
 
-    companion object {
-        const val NO_DATA_SOURCE_AVAILABLE = "no network or database data"
-        const val LOCATION_ERROR = "no location provided"
-        const val NETWORK_ERROR = "no network available"
-        const val UNKNOWN_ERROR = "no idea man"
-        const val FROM_DATABASE = "data came from database"
-        const val FROM_NETWORK = "data came from network"
-    }
+    const val NO_DATA_SOURCE_AVAILABLE = "no network or database data"
+    const val LOCATION_ERROR = "no location provided"
+    const val NETWORK_ERROR = "no network available"
+    const val UNKNOWN_ERROR = "no idea man"
+    const val FROM_DATABASE = "data came from database"
+    const val FROM_NETWORK = "data came from network"
 }
