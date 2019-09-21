4295effc87e59de1ea2bb6dedb1f8d602f8da895
Eric Diaz <ericdiaz@pursuit.org>
2019-09-21 14:58:28 -0400

    added app module and set up android injector for activity

---
+++ app/src/main/java/ericdiaz/program/topviewsightseeingcodingchallenge/di/WeatherApplication.kt
@@ -0,0 +1,28 @@
+package ericdiaz.program.topviewsightseeingcodingchallenge.di
+
+import android.app.Application
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import javax.inject.Inject
+
+class WeatherApplication : Application(), HasAndroidInjector {
+
+    @Inject
+    lateinit var androidInjector: DispatchingAndroidInjector<Any>
+
+    override fun onCreate() {
+        super.onCreate()
+
+        DaggerAppComponent.builder()
+            .application(this)
+            .build()
+            .inject(this)
+    }
+
+    override fun androidInjector(): AndroidInjector<Any> {
+        return androidInjector
+    }
+
+
+}
