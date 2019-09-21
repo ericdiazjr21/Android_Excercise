d8dc9d80f6c278396ad73e53b8c13403780ebe1a
Eric Diaz <ericdiaz@pursuit.org>
2019-09-21 18:41:11 -0400

    created weather activty with empty layout and permission activity

---
+++ app/src/main/java/ericdiaz/program/topviewsightseeingcodingchallenge/view/PermissionActivity.kt
@@ -0,0 +1,11 @@
+package ericdiaz.program.topviewsightseeingcodingchallenge.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PermissionActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/ericdiaz/program/topviewsightseeingcodingchallenge/view/WeatherActivity.kt
@@ -0,0 +1,13 @@
+package ericdiaz.program.topviewsightseeingcodingchallenge.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ericdiaz.program.topviewsightseeingcodingchallenge.R
+
+class WeatherActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather_layout)
+    }
+}
