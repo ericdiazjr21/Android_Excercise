898c8b60387679cbe019630f325eb7b67512d539
Eric Diaz <ericdiaz@pursuit.org>
2019-09-21 18:45:05 -0400

    forgot the annotations again

--- app/src/main/java/ericdiaz/program/topviewsightseeingcodingchallenge/viewmodel/di/ViewModelModule.kt
+++ app/src/main/java/ericdiaz/program/topviewsightseeingcodingchallenge/viewmodel/di/ViewModelModule.kt
@@ -1,12 +1,16 @@
 package ericdiaz.program.topviewsightseeingcodingchallenge.viewmodel.di
 
 import dagger.Module
+import dagger.Provides
 import ericdiaz.program.topviewsightseeingcodingchallenge.repository.WeatherNetworkRepository
 import ericdiaz.program.topviewsightseeingcodingchallenge.viewmodel.WeatherViewModelFactory
+import javax.inject.Singleton
 
 @Module
 class ViewModelModule {
 
+    @Provides
+    @Singleton
     fun providesWeatherViewModelFactory(weatherNetworkRepository: WeatherNetworkRepository): WeatherViewModelFactory {
         return WeatherViewModelFactory(weatherNetworkRepository)
     }
