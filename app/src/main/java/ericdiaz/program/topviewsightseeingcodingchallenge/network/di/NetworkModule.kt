9f2f51b77d9314575b182a30128b41d7a77a8380
Eric Diaz <ericdiaz@pursuit.org>
2019-09-21 14:29:10 -0400

    added network module with retrofit and logging

---
+++ app/src/main/java/ericdiaz/program/topviewsightseeingcodingchallenge/network/di/NetworkModule.kt
@@ -0,0 +1,49 @@
+package ericdiaz.program.topviewsightseeingcodingchallenge.network.di
+
+import dagger.Module
+import dagger.Provides
+import ericdiaz.program.topviewsightseeingcodingchallenge.network.WeatherService
+import io.reactivex.schedulers.Schedulers
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+class NetworkModule {
+
+    companion object {
+        private const val BASE_URL = "https://api.darksky.net"
+    }
+
+    @Provides
+    @Singleton
+    fun providesHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
+    }
+
+    @Provides
+    @Singleton
+    fun providesOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesWeatherService(retrofit: Retrofit): WeatherService {
+        return retrofit.create(WeatherService::class.java)
+    }
+}
