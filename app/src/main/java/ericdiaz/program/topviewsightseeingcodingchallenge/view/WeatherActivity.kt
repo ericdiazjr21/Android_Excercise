cf95d71b2b8764bca47fffdf03f4aea69994109b
Eric Diaz <ericdiaz@pursuit.org>
2019-09-22 00:58:42 -0400

    added data to views and view functions for showing and hiding views

--- app/src/main/java/ericdiaz/program/topviewsightseeingcodingchallenge/view/WeatherActivity.kt
+++ app/src/main/java/ericdiaz/program/topviewsightseeingcodingchallenge/view/WeatherActivity.kt
@@ -1,15 +1,18 @@
 package ericdiaz.program.topviewsightseeingcodingchallenge.view
 
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.material.snackbar.Snackbar
 import dagger.android.AndroidInjection
 import ericdiaz.program.topviewsightseeingcodingchallenge.R
 import ericdiaz.program.topviewsightseeingcodingchallenge.viewmodel.State
+import ericdiaz.program.topviewsightseeingcodingchallenge.viewmodel.StateDescriptor
 import ericdiaz.program.topviewsightseeingcodingchallenge.viewmodel.WeatherViewModel
 import ericdiaz.program.topviewsightseeingcodingchallenge.viewmodel.WeatherViewModelFactory
+import kotlinx.android.synthetic.main.activity_weather_layout.*
 import javax.inject.Inject
 
 class WeatherActivity : AppCompatActivity() {
@@ -29,10 +32,83 @@ class WeatherActivity : AppCompatActivity() {
         weatherViewModel.getWeatherData().observe(this,
             Observer<State> { state ->
                 when (state) {
-                    is State.Success -> Log.d("sasuke", state.weatherResponse.toString())
-                    is State.Failure -> Log.d("sasuke", state.throwable.toString())
-                    State.Loading -> TODO()
+                    is State.Success -> {
+                        hideProgressBar()
+
+                        hideNoDataTextView()
+
+                        loadDataIntoViews(state)
+
+                        showWeatherViews()
+                    }
+                    is State.Failure -> {
+                        if (state.stateDescriptor == StateDescriptor.NO_DATA_SOURCE_AVAILABLE) {
+                            showNoDataTextView()
+                        }
+                        hideProgressBar()
+
+                        showSnackBar(state.stateDescriptor)
+                    }
+                    is State.Loading -> {
+                        hideWeatherViews()
+
+                        hideNoDataTextView()
+
+                        showProgressBar()
+                    }
                 }
             })
     }
+
+    private fun loadDataIntoViews(state: State.Success) {
+
+        if (state.stateDescriptor == StateDescriptor.FROM_DATABASE) {
+            showSnackBar(state.stateDescriptor)
+        }
+
+        val weatherResponse = state.weatherResponse
+        val currentWeather = weatherResponse.currentWeather
+
+        current_location_text_view.text = weatherResponse.location
+        last_updated_text_view.text = currentWeather.date
+        current_temperature_text_view.text = currentWeather.temperature.toString()
+    }
+
+
+    private fun showWeatherViews() {
+        current_location_text_view.visibility = View.VISIBLE
+        current_temperature_text_view.visibility = View.VISIBLE
+        last_updated_text_view.visibility = View.VISIBLE
+        current_weather_icon_image_view.visibility = View.VISIBLE
+        daily_forecast_recycler_view.visibility = View.VISIBLE
+    }
+
+    private fun hideWeatherViews() {
+        current_location_text_view.visibility = View.INVISIBLE
+        current_temperature_text_view.visibility = View.INVISIBLE
+        last_updated_text_view.visibility = View.INVISIBLE
+        current_weather_icon_image_view.visibility = View.INVISIBLE
+        daily_forecast_recycler_view.visibility = View.INVISIBLE
+    }
+
+    private fun showNoDataTextView() {
+        no_data_text_view.visibility = View.VISIBLE
+    }
+
+    private fun hideNoDataTextView() {
+        no_data_text_view.visibility = View.INVISIBLE
+    }
+
+    private fun showProgressBar() {
+        loading_progress_bar.visibility = View.VISIBLE
+    }
+
+    private fun hideProgressBar() {
+        loading_progress_bar.visibility = View.INVISIBLE
+    }
+
+    private fun showSnackBar(message: String) {
+        Snackbar.make(loading_progress_bar, message, Snackbar.LENGTH_LONG)
+    }
+
 }
