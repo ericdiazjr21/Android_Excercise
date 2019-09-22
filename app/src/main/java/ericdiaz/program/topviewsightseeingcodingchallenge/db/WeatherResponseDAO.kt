b5efbc55d02035807b7ac499bf6443358f5ccbed
Eric Diaz <ericdiaz@pursuit.org>
2019-09-22 12:28:57 -0400

    created weather response Dao

---
+++ app/src/main/java/ericdiaz/program/topviewsightseeingcodingchallenge/db/WeatherResponseDAO.kt
@@ -0,0 +1,19 @@
+package ericdiaz.program.topviewsightseeingcodingchallenge.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import ericdiaz.program.topviewsightseeingcodingchallenge.model.WeatherResponse
+import io.reactivex.Single
+
+@Dao
+interface WeatherResponseDAO {
+
+    @Query("SELECT * FROM weatherForecastTable")
+    fun getCachedWeatherResponse(): Single<WeatherResponse>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertWeatherResponse(weatherResponse: WeatherResponse)
+
+}
