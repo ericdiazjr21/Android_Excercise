51327bbb587d09a16ab0394865253322c4fe418c
Eric Diaz <ericdiaz@pursuit.org>
2019-09-22 12:44:47 -0400

    added type converters for weather response properties

--- app/src/main/java/ericdiaz/program/topviewsightseeingcodingchallenge/db/WeatherDatabase.kt
+++ app/src/main/java/ericdiaz/program/topviewsightseeingcodingchallenge/db/WeatherDatabase.kt
@@ -2,9 +2,12 @@ package ericdiaz.program.topviewsightseeingcodingchallenge.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import ericdiaz.program.topviewsightseeingcodingchallenge.db.util.WeatherDatabaseTypeConverters
 import ericdiaz.program.topviewsightseeingcodingchallenge.model.WeatherResponse
 
 @Database(entities = [WeatherResponse::class], version = 1, exportSchema = false)
+@TypeConverters(WeatherDatabaseTypeConverters::class)
 abstract class WeatherDatabase : RoomDatabase() {
 
     abstract fun getWeatherResponseDao(): WeatherResponseDAO
