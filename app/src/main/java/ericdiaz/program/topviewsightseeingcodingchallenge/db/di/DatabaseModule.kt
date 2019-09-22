76f0a2e1eef4b8bbe0666a010e13cfaa5a8f6731
Eric Diaz <ericdiaz@pursuit.org>
2019-09-22 12:50:50 -0400

    added weather database di package with database module

---
+++ app/src/main/java/ericdiaz/program/topviewsightseeingcodingchallenge/db/di/DatabaseModule.kt
@@ -0,0 +1,30 @@
+package ericdiaz.program.topviewsightseeingcodingchallenge.db.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import ericdiaz.program.topviewsightseeingcodingchallenge.db.WeatherDatabase
+import ericdiaz.program.topviewsightseeingcodingchallenge.db.WeatherResponseDAO
+import javax.inject.Singleton
+
+@Module
+class DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun providesWeatherDatabase(context: Context): WeatherDatabase {
+        synchronized(WeatherDatabase::class) {
+            return Room
+                .databaseBuilder(context, WeatherDatabase::class.java, "WeatherForecast.db")
+                .build()
+        }
+    }
+
+
+    @Singleton
+    @Provides
+    fun providesWeatherResponseDAO(weatherDatabase: WeatherDatabase): WeatherResponseDAO {
+        return weatherDatabase.getWeatherResponseDao()
+    }
+}
