5e12769d7bf638c040cc8effc5972c26ccbea550
Eric Diaz <ericdiaz@pursuit.org>
2019-09-22 13:02:05 -0400

    added weather database to di in repo package

--- app/src/main/java/ericdiaz/program/topviewsightseeingcodingchallenge/repository/di/RepositoryModule.kt
+++ app/src/main/java/ericdiaz/program/topviewsightseeingcodingchallenge/repository/di/RepositoryModule.kt
@@ -2,7 +2,9 @@ package ericdiaz.program.topviewsightseeingcodingchallenge.repository.di
 
 import dagger.Module
 import dagger.Provides
+import ericdiaz.program.topviewsightseeingcodingchallenge.db.WeatherResponseDAO
 import ericdiaz.program.topviewsightseeingcodingchallenge.network.WeatherService
+import ericdiaz.program.topviewsightseeingcodingchallenge.repository.WeatherDatabaseRepository
 import ericdiaz.program.topviewsightseeingcodingchallenge.repository.WeatherNetworkRepository
 import javax.inject.Singleton
 
@@ -13,5 +15,11 @@ class RepositoryModule {
     @Provides
     fun providesWeatherNetworkRepository(weatherService: WeatherService): WeatherNetworkRepository {
         return WeatherNetworkRepository(weatherService)
+    }
+
+    @Singleton
+    @Provides
+    fun providesWeatherDatabaseRepository(weatherResponseDAO: WeatherResponseDAO): WeatherDatabaseRepository {
+        return WeatherDatabaseRepository(weatherResponseDAO)
     }
 }
